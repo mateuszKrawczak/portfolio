97918d163e65393437655993593a29a71842e98c
matson19 <241318@student.pwr.edu.pl>
2020-06-07 18:51:08 +0200

    prototype of layout added

--- src/app/portfolio/main-content/projects/projects.component.ts
+++ src/app/portfolio/main-content/projects/projects.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss','../about-me/about-me.component.scss']
 })
 export class ProjectsComponent implements OnInit {
 
--- src/app/portfolio/main-content/technologies/technologies.component.ts
+++ src/app/portfolio/main-content/technologies/technologies.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-technologies',
   templateUrl: './technologies.component.html',
-  styleUrls: ['./technologies.component.scss']
+  styleUrls: ['./technologies.component.scss','../about-me/about-me.component.scss']
 })
 export class TechnologiesComponent implements OnInit {
 
