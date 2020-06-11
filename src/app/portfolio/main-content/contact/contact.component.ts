ddd61c0b10ea7b922ab564a49bf9eaacfbfc7734
matson19 <241318@student.pwr.edu.pl>
2020-06-11 15:58:39 +0200

    fixed layout on site, contact section done, only galerry of projects to do and welcome section

--- src/app/portfolio/main-content/contact/contact.component.ts
+++ src/app/portfolio/main-content/contact/contact.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import{faFacebookF, faLinkedinIn,faGithub, faInstagram} from '@fortawesome/free-brands-svg-icons'
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss','../about-me/about-me.component.scss']
 })
 export class ContactComponent implements OnInit {
+
+  iconFacebook = faFacebookF;
+  iconLinkedin = faLinkedinIn;
+  iconGithub = faGithub;
+  iconInstagram = faInstagram;
 
   constructor() { }
 
--- src/app/portfolio/main-content/projects/projects.component.ts
+++ src/app/portfolio/main-content/projects/projects.component.ts
@@ -26,7 +26,7 @@ export class ProjectsComponent implements OnInit {
     this.projectExample.photos=["neon.png",""];
     this.projectExample.link="https://github.com/mateuszKrawczak/kpz-restaurant-frontend";
     this.projects[0]=this.projectExample;
-    this.projects[1]={name:"Easy quisy",description:"This is a quiz app which helps you check your real knowledge. Quiz has six categories with three difficulty levels with really tough questions.",photos:["neon.png"],link:"https://github.com/mateuszKrawczak/EducationalApp"};
+    this.projects[1]={name:"Easy quisy",description:"This is a quiz app which helps you check your real knowledge. Quiz has six categories with three difficulty levels with really tough questions.",photos:["EQ_home_screen.png","EQ_kategorie_screen.png","EQ_pytanie_screen.png"],link:"https://github.com/mateuszKrawczak/EducationalApp"};
     this.projects[2]={name:"City site",description:"This is a simple website about London City. On this site you can check famous attractions and monuments or check current weather in London.",photos:["blackWood.png"],link:"https://github.com/mateuszKrawczak/Website"};
     
   }
